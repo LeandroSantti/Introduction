23636380148d92e0b4d0c0d45eb7d79816843caa
LeandroSantti <santti.leandro@gmail.com>
2019-02-19 02:29:28 -0300

    Exercise_1 modified

--- Ejercicio_1/Ejercicio_1/Ejercicio_1/Program.cs
+++ Ejercicio_1/Ejercicio_1/Ejercicio_1/Program.cs
@@ -11,61 +11,33 @@ namespace Ejercicio_1
         static void Main(string[] args)
         {// 1. Ingresar 5 números por consola, guardándolos en una variable escalar.
          // Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio. 
-            int num1, num2, num3, num4, num5;
-            Console.WriteLine("Digite un número:");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite un número:");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite un número:");
-            num3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite un número:");
-            num4 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite un número:");
-            num5 = Convert.ToInt32(Console.ReadLine());
-            
-            if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5)
+            int cantidad = 5, num, max =0, min=0, promedio=0; //Preguntar a Demian porque tuve que igualarlas a cero. Tiraba un error.
+            Console.WriteLine("Digite sus cinco números.");
+            Console.Clear();
+            for (int x = 0; x < cantidad; x++)
             {
-                Console.WriteLine("Numero mayor: " + num1);
+                Console.WriteLine("Digite un número:");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (x == 0)
+                {
+                    max = num;
+                    min = num;
+                }
+                
+                    if (num < min)
+                    {
+                        min = num;
+                    }
+                    if (num > max)
+                    {
+                        max = num;
+                    }
+                promedio =  (promedio + num); //Salió de casualidad, ¿Por qué se acumulan los números si en ningún lado los sumé entre si?
             }
-            if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5)
-            {
-                Console.WriteLine("Numero mayor: " + num2);
-            }
-            if (num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5)
-            {
-                Console.WriteLine("Numero mayor: " + num3);
-            }
-            if (num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5)
-            {
-                Console.WriteLine("Numero mayor: " + num4);
-            }
-            if (num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4)
-            {
-                Console.WriteLine("Numero mayor: " + num5);
-            }
-            if (num1 < num2 && num1 < num3 && num1 < num4 && num1 < num5)
-            {
-                Console.WriteLine("Numero menor: " + num1);
-            }
-            if (num2 < num1 && num2 < num3 && num2 < num4 && num2 < num5)
-            {
-                Console.WriteLine("Numero menor: " + num2);
-            }
-            if (num3 < num1 && num3 < num2 && num3 < num4 && num3 < num5)
-            {
-                Console.WriteLine("Numero menor: " + num3);
-            }
-            if (num4 < num1 && num4 < num2 && num4 < num3 && num4 < num5)
-            {
-                Console.WriteLine("Numero menor: " + num4);
-            }
-            if (num5 < num1 && num5 < num2 && num5 < num3 && num5 < num4)
-            {
-                Console.WriteLine("Numero menor: " + num5);
-            }
-            Console.WriteLine("El promedio es: " + (num1 + num2 + num3 + num4 + num5) / 5);
-            Console.ReadKey();
-            
+            Console.WriteLine("El número máximo es: " + max);
+            Console.WriteLine("El número mínimo es: " + min);
+            Console.WriteLine("El promedio de los números es: " + promedio / cantidad);
+            Console.ReadKey();    
         }
     }
 }
