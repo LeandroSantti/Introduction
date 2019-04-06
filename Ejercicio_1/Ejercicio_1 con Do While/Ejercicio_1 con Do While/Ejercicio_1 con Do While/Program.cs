6887f42abb3afffb5c131fc80535153e1f6454a7
demian44 <demi-44@hotmail.com>
2019-04-06 15:33:43 -0300

    Fixed the max/min logic

--- Ejercicio_1/Ejercicio_1 con Do While/Ejercicio_1 con Do While/Ejercicio_1 con Do While/Program.cs
+++ Ejercicio_1/Ejercicio_1 con Do While/Ejercicio_1 con Do While/Ejercicio_1 con Do While/Program.cs
@@ -13,27 +13,34 @@ namespace Ejercicio_1_con_Do_While
          // Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio
             int cantidad = 0, max = 0, min =0, num, promedio = 0;
             Console.WriteLine("Digite sus 5 números:");
-           
+
             do
             {
                 num = Convert.ToInt32(Console.ReadLine());
-                cantidad++;
-                if (num == 0)
+                
+                if (cantidad == 0)
                 {
                     num = max;
                     num = min;
                 }
-                if (num > max)
+                else 
                 {
-                    max = num;
+                    if (num > max)
+                    {
+                        max = num;
+                    }
+                    
+                    if (num < min)
+                    {
+                        min = num;
+                    }
                 }
-                if (num < min)
-                {
-                    min = num;
-                }
+                
+                cantidad++;
                 promedio = (promedio + num);
-            }
+            }            
             while (cantidad < 5);
+            
             Console.WriteLine("El número máximo es: " + max);
             Console.WriteLine("El número mínimo es: " + min);
             Console.WriteLine("El promedio de los números es: " + promedio / cantidad);
